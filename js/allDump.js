c72f07039445f3c17c3d657978eae1c4fd6947b2
nitheeshmashraf <nitheeshmashraf@gmail.com>
2019-04-09 11:35:20 +0400

    normalise and all dump

--- js/allDump.js
+++ js/allDump.js
@@ -15,3 +15,16 @@ function copyToClipboard(flagg) {
             document.execCommand("copy");
             document.body.removeChild(dummy);
         }
+
+
+        function copynewtoClipboard () {
+            var text = "You failed to click on the text";
+            text = document.getElementById("new_Sentence").textContent;
+            var dummy = document.createElement("input");
+            document.body.appendChild(dummy);
+            dummy.setAttribute('value', text);
+            dummy.select();
+            document.execCommand("copy");
+            document.body.removeChild(dummy);
+    
+        }
